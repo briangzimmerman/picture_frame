5fd4ea4460308f37b8638e213842a610271052e0
Brian Zimmerman <briangzimmerman@gmail.com>
2020-05-20 18:13:18 -0500

    Issue #17: Add default display

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ sys.path.append(os.path.dirname('.'))
 from components.jobs.job_manager import JobManager
 from components.utils.utils import create_object
 
+if os.environ.get('DISPLAY', '') == '':
+    os.environ.__setitem__('DISPLAY', ':0.0')
+
 config = yaml.load(open('./config.yaml'), Loader=yaml.UnsafeLoader)
 jobs   = []
 
