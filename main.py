339df702ee5875c02633c405b95ddf6b2c894191
Brian Zimmerman <briangzimmerman@gmail.com>
2020-05-20 18:23:28 -0500

    Issue #18: Fix file path in main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import yaml
 import sys
 import os
-sys.path.append(os.path.dirname('.'))
+
+currentDirectory = os.path.dirname(__file__)
+
+if not currentDirectory:
+    currentDirectory = '.'
+
+sys.path.append(currentDirectory)
 
 from components.jobs.job_manager import JobManager
 from components.utils.utils import create_object
 
-if os.environ.get('DISPLAY', '') == '':
-    os.environ.__setitem__('DISPLAY', ':0.0')
+# if os.environ.get('DISPLAY', '') == '':
+#     os.environ.__setitem__('DISPLAY', ':0.0')
 
-config = yaml.load(open('./config.yaml'), Loader=yaml.UnsafeLoader)
+config = yaml.load(open(currentDirectory + '/config.yaml'), Loader=yaml.UnsafeLoader)
 jobs   = []
 
 for job in config['jobs']:
