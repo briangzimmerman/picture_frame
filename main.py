db21e1f5380c1e3c8ec124304171fc92ea49a231
Brian Zimmerman <briangzimmerman@gmail.com>
2020-05-20 18:30:55 -0500

    Issue #19: Add default display
    Uncommenting code block

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ sys.path.append(currentDirectory)
 from components.jobs.job_manager import JobManager
 from components.utils.utils import create_object
 
-# if os.environ.get('DISPLAY', '') == '':
-#     os.environ.__setitem__('DISPLAY', ':0.0')
+if os.environ.get('DISPLAY', '') == '':
+    os.environ.__setitem__('DISPLAY', ':0.0')
 
 config = yaml.load(open(currentDirectory + '/config.yaml'), Loader=yaml.UnsafeLoader)
 jobs   = []
